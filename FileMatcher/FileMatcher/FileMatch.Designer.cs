687b77ee0ddd6e3dc0aab97cb855f18361f9aaab
Andrew Collington <andy@amnuts.com>
2013-03-09 06:49:55 +0000

    Initial import

---
+++ FileMatcher/FileMatcher/FileMatch.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace FileMatcher
+{
+    partial class FileMatchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prgMatching = new System.Windows.Forms.ProgressBar();
+            this.dlogDirSelect = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnDirSelect = new System.Windows.Forms.Button();
+            this.txtDirPath = new System.Windows.Forms.TextBox();
+            this.cbkMatchRecursive = new System.Windows.Forms.CheckBox();
+            this.btnFindMatches = new System.Windows.Forms.Button();
+            this.treeResults = new System.Windows.Forms.TreeView();
+            this.picMatch1 = new System.Windows.Forms.PictureBox();
+            this.picMatch2 = new System.Windows.Forms.PictureBox();
+            this.labelInfoLine = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picMatch1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMatch2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // prgMatching
+            // 
+            this.prgMatching.Location = new System.Drawing.Point(622, 443);
+            this.prgMatching.Name = "prgMatching";
+            this.prgMatching.Size = new System.Drawing.Size(100, 17);
+            this.prgMatching.Step = 1;
+            this.prgMatching.TabIndex = 1;
+            this.prgMatching.Visible = false;
+            // 
+            // btnDirSelect
+            // 
+            this.btnDirSelect.Location = new System.Drawing.Point(13, 13);
+            this.btnDirSelect.Name = "btnDirSelect";
+            this.btnDirSelect.Size = new System.Drawing.Size(86, 23);
+            this.btnDirSelect.TabIndex = 2;
+            this.btnDirSelect.Text = "Select a folder";
+            this.btnDirSelect.UseVisualStyleBackColor = true;
+            this.btnDirSelect.Click += new System.EventHandler(this.ChooseFolder);
+            // 
+            // txtDirPath
+            // 
+            this.txtDirPath.Location = new System.Drawing.Point(13, 42);
+            this.txtDirPath.Name = "txtDirPath";
+            this.txtDirPath.Size = new System.Drawing.Size(482, 20);
+            this.txtDirPath.TabIndex = 3;
+            // 
+            // cbkMatchRecursive
+            // 
+            this.cbkMatchRecursive.AutoSize = true;
+            this.cbkMatchRecursive.Location = new System.Drawing.Point(426, 17);
+            this.cbkMatchRecursive.Name = "cbkMatchRecursive";
+            this.cbkMatchRecursive.Size = new System.Drawing.Size(69, 17);
+            this.cbkMatchRecursive.TabIndex = 4;
+            this.cbkMatchRecursive.Text = "recursive";
+            this.cbkMatchRecursive.UseVisualStyleBackColor = true;
+            // 
+            // btnFindMatches
+            // 
+            this.btnFindMatches.Enabled = false;
+            this.btnFindMatches.Location = new System.Drawing.Point(509, 13);
+            this.btnFindMatches.Name = "btnFindMatches";
+            this.btnFindMatches.Size = new System.Drawing.Size(212, 49);
+            this.btnFindMatches.TabIndex = 5;
+            this.btnFindMatches.Text = "Find matches";
+            this.btnFindMatches.UseVisualStyleBackColor = true;
+            this.btnFindMatches.Click += new System.EventHandler(this.FindMatches);
+            // 
+            // treeResults
+            // 
+            this.treeResults.Enabled = false;
+            this.treeResults.Location = new System.Drawing.Point(13, 69);
+            this.treeResults.Name = "treeResults";
+            this.treeResults.Size = new System.Drawing.Size(482, 361);
+            this.treeResults.TabIndex = 6;
+            // 
+            // picMatch1
+            // 
+            this.picMatch1.Enabled = false;
+            this.picMatch1.InitialImage = null;
+            this.picMatch1.Location = new System.Drawing.Point(509, 69);
+            this.picMatch1.Name = "picMatch1";
+            this.picMatch1.Size = new System.Drawing.Size(212, 178);
+            this.picMatch1.TabIndex = 7;
+            this.picMatch1.TabStop = false;
+            // 
+            // picMatch2
+            // 
+            this.picMatch2.Enabled = false;
+            this.picMatch2.InitialImage = null;
+            this.picMatch2.Location = new System.Drawing.Point(509, 252);
+            this.picMatch2.Name = "picMatch2";
+            this.picMatch2.Size = new System.Drawing.Size(212, 178);
+            this.picMatch2.TabIndex = 8;
+            this.picMatch2.TabStop = false;
+            // 
+            // labelInfoLine
+            // 
+            this.labelInfoLine.AutoSize = true;
+            this.labelInfoLine.Location = new System.Drawing.Point(11, 445);
+            this.labelInfoLine.MinimumSize = new System.Drawing.Size(248, 13);
+            this.labelInfoLine.Name = "labelInfoLine";
+            this.labelInfoLine.Size = new System.Drawing.Size(248, 13);
+            this.labelInfoLine.TabIndex = 9;
+            this.labelInfoLine.Text = "Searching may take a long time - please be patient!";
+            // 
+            // FileMatchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 472);
+            this.Controls.Add(this.labelInfoLine);
+            this.Controls.Add(this.picMatch2);
+            this.Controls.Add(this.picMatch1);
+            this.Controls.Add(this.treeResults);
+            this.Controls.Add(this.btnFindMatches);
+            this.Controls.Add(this.cbkMatchRecursive);
+            this.Controls.Add(this.txtDirPath);
+            this.Controls.Add(this.btnDirSelect);
+            this.Controls.Add(this.prgMatching);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FileMatchForm";
+            this.Text = "File Matcher";
+            ((System.ComponentModel.ISupportInitialize)(this.picMatch1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMatch2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgMatching;
+        private System.Windows.Forms.FolderBrowserDialog dlogDirSelect;
+        private System.Windows.Forms.Button btnDirSelect;
+        private System.Windows.Forms.TextBox txtDirPath;
+        private System.Windows.Forms.CheckBox cbkMatchRecursive;
+        private System.Windows.Forms.Button btnFindMatches;
+        private System.Windows.Forms.TreeView treeResults;
+        private System.Windows.Forms.PictureBox picMatch1;
+        private System.Windows.Forms.PictureBox picMatch2;
+        private System.Windows.Forms.Label labelInfoLine;
+
+    }
+}
+
---
+++ FileMatcher/FileMatcher/FileMatch.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace FileMatcher
+{
+    public partial class FileMatchForm : Form
+    {
+        private bool isRecursive;
+
+        public FileMatchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFolder(object sender, EventArgs e)
+        {
+            if (dlogDirSelect.ShowDialog() == DialogResult.OK)
+            {
+                txtDirPath.Text = dlogDirSelect.SelectedPath;
+                btnFindMatches.Enabled = true;
+            }
+        }
+
+        private void FindMatches(object sender, EventArgs e)
+        {
+            isRecursive = cbkMatchRecursive.Checked;
+            string path = txtDirPath.Text;
+            prgMatching.Value = 0;
+            labelInfoLine.Text = "Searching may take a long time - please be patient!";
+
+            try
+            {
+                var files = from file 
+                    in Directory.EnumerateFiles(path, "*.jpg", (isRecursive 
+                        ? SearchOption.AllDirectories 
+                        : SearchOption.TopDirectoryOnly)
+                    )
+                    select new { File = file };
+                int total = files.Count();
+                prgMatching.Maximum = total;
+                prgMatching.Visible = true;
+                labelInfoLine.Text = String.Format("{0} file{1} found", total.ToString(), (total == 1 ? "" : "s"));
+                if (total < 2)
+                {
+                    labelInfoLine.Text += " - nothing with which to compare";
+                }
+                else
+                {
+                    labelInfoLine.Text += " - processing...";
+                    foreach (var f in files)
+                    {
+                        string md5 = ProcessFile(f.File);
+                        prgMatching.PerformStep();
+                    }
+                    labelInfoLine.Text = String.Format("{0} files processed", total.ToString());
+                }
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Debug.WriteLine(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Debug.WriteLine(PathEx.Message);
+            }
+
+
+
+            /*
+            if (Directory.Exists(path))
+            {
+                // This path is a directory
+                ProcessDirectory(path);
+            }
+            else
+            {
+                Debug.WriteLine("{0} is not a valid file or directory.", txtDirPath);
+            }
+            */
+        }
+
+        /*
+        public void ProcessDirectory(string targetDirectory)
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+            {
+                ProcessFile(fileName);
+            }
+
+            if (isRecursive)
+            {
+                string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+                foreach (string subdirectory in subdirectoryEntries)
+                {
+                    ProcessDirectory(subdirectory);
+                }
+            }
+        }
+        */
+
+        public string ProcessFile(string path)
+        {
+            FileStream file = new FileStream(path, FileMode.Open);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(file);
+            file.Close();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            Debug.WriteLine("Processed file '{0}'.", path);
+            return sb.ToString();
+        }
+
+    }
+}
